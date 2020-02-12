bfeb535f13403f8a79a57382ca426b8773d9f841
Alastair-G <60227283+Alastair-G@users.noreply.github.com>
2020-02-12 16:54:10 +0000

    Add files via upload

--- electricField.cc
+++ electricField.cc
@@ -11,34 +11,50 @@ int main() {
 
 	using namespace std;
 
-	int x_values=40, y_values=40; //Defines the number of x and y, grid of size (x_values)*(y_values)
-
-	float x_0 = 0; //The starting x coordinate in space
-
-	float y_0 = 0; //The starting y coordinate in space
-
-	float h=0.5; //Defines the separation of the points (stepsize) in the grid
-
+	int x_values=100, y_values=100; //Defines the number of x and y, grid of size (x_values)*(y_values)
+	float x_0 = 0; //Optional coordinate offset
+	float y_0 = 0; //Optional coordinate offset
+	float h = 1.0; //Defines the separation of the points (stepsize) in the grid
 	float V[x_values][y_values]; //Defines array to store the values of the potential
-	
-
-	//SECTION OF CODE TO READ IN THE POTENTIAL FROM FILE
-
-
 	float E[x_values][y_values][2]; //Initialises array to store E-field values, E[][][0] x component and E[][][1] y component of E-field
+	float magE=0; //Defines variable to store the |E|
+	float maxE=0, minE=100; //Defines the maximum and minimum values of |E|
+	
+	
+	//IMPORT OF POTENTIAL DATA 
+
+	int i, j, iter, y;
+
+	for(i=0; i<x_values; i++)                 /* clear the array  */
+	  {   
+	    for (j=0; j<y_values; j++) V[i][j] = 0;
+	  }
+
+	for(i=0; i<x_values; i++) V[i][0] = 100;        /* p[i][0] = 100 V */		
+	for(i=0; i<x_values; i++) V[i][y_values-1] = -100;
+
+
+	for (iter=0; iter<1000; iter++) {
+	  for (i=1; i<x_values-1; i++) {
+	    for (j=1; j<y_values-1; j++) {
+	      V[i][j] = 0.25*(V[i+1][j] + V[i-1][j] + V[i][j+1] + V[i][j-1]);
+	    }
+	  }
+	}
+
 
 	ofstream outFile;
-  	outFile.open("electricField.txt"); //opens file to write data to
+  	outFile.open("electricField.dat"); //Opens file to write data to
 
-  	 outFile << "x Pos \ty Pos \tE_x \tE_y" << endl; //writes headings in the output file
+	for (int x=0; x<x_values-1; x++) { //Loops over x positions
 
-	for (int x=0; x<x_values; x++) { //Loops over x positions
+		for (int y=0; y<y_values-1; y++) { //Loops over y positions
+		  
+			//OPTIONAL IF STATEMENT SHOULD BE ADDED HERE TO DEFINE REGIONS TO CALCULATE E-FIELD
 
-		for (int y=0; y<y_values; y++) { //Loops over y positions
+			//CALCULATION OF THE X-COMPONENTS OF THE E-FIELD
 
-			//Calculation of x components of the E-field
-
-			if ( x==0 ) { //If on left boundary of grid
+		  	if ( x == 0 ) { //If on left boundary of grid
 				E[x][y][0] = -(V[x+1][y] - V[x][y])/h; //Finite difference using point to the right
 			}
 
@@ -48,28 +64,43 @@ int main() {
 
 			else { //If on central point of grid
 				E[x][y][0] = -(V[x+1][y] - V[x-1][y])/(2*h); //Central difference method
-			}
+				}
+   
+		  
+		  	//CALCULATION OF THE Y-COMPONENTS OF THE E-FIELD
 
-			//Calculation of y components of the E-field
-
-			if ( y == 0 ) { //If on upper boundary of grid
+		        if ( y == 0 ) { //If on lower boundary of grid
 				E[x][y][1] = -(V[x][y+1] - V[x][y])/h; //Finite difference using point below
 			}
 
-			else if ( y == (y_values-1) ) { //If on lower boundary of grid
-				E[x][y][1] = -(V[x][y] - V[x][y-1])/h; //Finite difference using point from above
+			else if ( y == (y_values-1) ) { //If on upper boundary of grid
+				E[x][y][1] = 0-(V[x][y] - V[x][y-1])/h; //Finite difference using point from above
 			}
-
+			
 			else { //If on central point of grid
-				E[x][y][1] = -(V[x][y+1] - V[x][y-1])/(2*h); //Central difference method
+			  	E[x][y][1] = -(V[x][y+1] - V[x][y-1])/(2*h); //Central difference method
 			}
 
-			outFile << x_0+x*h << "\t"  << y_0+y*h << "\t" << V[x][y] << "\t" << E[x][y][0] << "\t" << E[x][y][1] << endl; //outputs data to output file
+			magE = ((E[x][y][0])*(E[x][y][0])+ (E[x][y][1])*(E[x][y][1]));
+
+			//DETERMINATION OF MAX/MIN VALUES OF |E|
+
+			if ( ((E[x][y][0])*(E[x][y][0])+ (E[x][y][1])*(E[x][y][1]))<minE ) { //Checks for new minimum value of |E|
+				minE = magE; //Sets new minimum value of |E|
+			}
+
+			if ( ((E[x][y][0])*(E[x][y][0])+ (E[x][y][1])*(E[x][y][1]))>maxE ) { //Checks for new maximum value of |E|
+				maxE = magE; //Sets new maximum value of |E|
+			}
+			
+			outFile << x_0+x*h << "\t" << y_0+y*h  << "\t" << E[x][y][0] << "\t" << E[x][y][1]  << endl; //outputs data to output file
 
 		}
 	}
 
 	outFile.close(); //closes the output file
+
+	cout << "Max |E|: " << maxE << "\nMin |E|: " << minE << endl; //Outputs maximum/minimum values of |E| to terminal
 
 	return 0;
 }
